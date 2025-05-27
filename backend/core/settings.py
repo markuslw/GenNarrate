5c8b822efef38cd003546b16e71663ce8ca26ce9
Markus Wilhelmsen <markuslw10@icloud.com>
2025-05-27 02:10:44 +0200

    added model functionality

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -53,9 +53,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'core.urls'
 
---
+++ inference/main.py
@@ -0,0 +1,51 @@
+from flask import Flask, request, jsonify
+import fitz
+import torch
+from torch.serialization import add_safe_globals
+from TTS.tts.configs.xtts_config import XttsConfig
+from TTS.tts.models.xtts import XttsAudioConfig, XttsArgs
+from TTS.config.shared_configs import BaseDatasetConfig
+from transformers import AutoTokenizer, AutoModelForCausalLM
+
+app = Flask(__name__)
+
+model_id = "deepseek-ai/deepseek-llm-7b-chat"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    device_map="auto",
+    torch_dtype=torch.float16
+)
+model.eval()
+
+@app.route("/")
+def index():
+    return jsonify({"message": "Welcome to the Question Answering API!"})
+
+@app.route("/generateTextString", methods=["POST"])
+def text_to_text():
+    data = request.get_json()
+    text = data["text"]
+
+    inputs = tokenizer(text, return_tensors="pt").to(model.device)
+    with torch.no_grad():
+        outputs = model.generate(**inputs, max_new_tokens=700, temperature=0.8)
+    response = tokenizer.decode(outputs[0], skip_special_tokens=True)
+
+    return jsonify({"response": response})
+
+@app.route("/generateVoiceAudio", methods=["POST"])
+def text_to_speech():
+    tts = TTS(model_name="tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
+
+    tts.tts_to_file(
+        text=ai_text,
+        file_path="output2.wav",
+        speaker_wav="female.wav",
+        language="en"
+    )
+    
+    return jsonify({"message": "Audio generated successfully!"})
+
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
